c0a1d9efdf728d68a41ddbde40ff617fb155774d
imanali <imanali.cse@gmail.com>
2017-07-08 17:03:53 +0600

    template driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormsComponent
   ],
   imports: [
     BrowserModule,
