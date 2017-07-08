2db48ac5ddf2b05ec0e9f6b8bd00bfb0f99d3ea8
imanali <imanali.cse@gmail.com>
2017-07-08 17:34:17 +0600

    template driven forms routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TemplateDrivenFormsComponent } from './forms/template-driven-forms/template-driven-forms.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'forms/template-driven-forms',
+        component: TemplateDrivenFormsComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TemplateDrivenFormsComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
