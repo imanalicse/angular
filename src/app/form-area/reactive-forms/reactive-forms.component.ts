81209b4424c0609516de6930df0ede9ac0fe10c5
imanali <imanali.cse@gmail.com>
2017-07-10 00:51:03 +0600

    reactve form validations

--- src/app/form-area/reactive-forms/reactive-forms.component.ts
+++ src/app/form-area/reactive-forms/reactive-forms.component.ts
@@ -11,18 +11,21 @@ export class ReactiveFormsComponent implements OnInit {
   formGroup: FormGroup;
 
   userForm = new FormGroup({
-    name: new FormControl('', Validators.required),
+    name: new FormControl('Iman', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
     email: new FormControl(),
     address: new FormGroup({
       street: new FormControl(),
       city: new FormControl(),
-      postalCode: new FormControl()
+      postalCode: new FormControl(null, Validators.pattern('^[0-9][0-9]{4}$'))
     })
   });
+
+
 
   constructor() { }
 
   ngOnInit() {
+    //console.log(this.userForm.controls['name'].hasError('required'))
   }
 
   onSubmit(){
