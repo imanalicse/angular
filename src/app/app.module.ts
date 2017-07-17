953a634f2700bf5b1c2e6b9308bb45fce0ecbb71
iman <iman@bitmascot.com>
2017-07-17 18:44:24 +0600

    httpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
-import {HttpModule, HttpClientModule} from "@angular/http";
+import {HttpModule} from "@angular/http";
 import {RouterModule} from "@angular/router";
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdPaginatorModule, MdInputModule} from '@angular/material';
