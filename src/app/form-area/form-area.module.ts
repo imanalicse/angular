3818e61ad5fbf87ecf067645801773f967632233
iman <iman@bitmascot.com>
2017-07-17 10:37:12 +0600

    router module

--- src/app/form-area/form-area.module.ts
+++ src/app/form-area/form-area.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+
 import {FormAreaRoutingModule} from "./form-area-routing.module";
 import {FormAreaComponent} from "./form-area.component";
 import {TemplateDrivenFormsComponent} from "./template-driven-forms/template-driven-forms.component";
@@ -12,6 +14,7 @@ import {ReactiveFormsComponent} from "./reactive-forms/reactive-forms.component"
     imports: [
         FormsModule,
         BrowserModule,
+        RouterModule,
         CommonModule,
         ReactiveFormsModule,
         FormAreaRoutingModule
